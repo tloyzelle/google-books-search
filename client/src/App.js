d6e56059dc2398c772d9e1b18e243bb54d7b4b25
Taegan Loyzelle <tloyzelle@gmail.com>
2021-10-02 11:30:05 -0400

    adding to move throughout pages of application

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import NoMatch from "./pages/NoMatch";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
