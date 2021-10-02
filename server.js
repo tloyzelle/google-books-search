b1ecca1830db7af0f56e7cd7f4f4b2b49cca0b84
Taegan Loyzelle <tloyzelle@gmail.com>
2021-10-02 11:55:38 -0400

    editing for heroku deployment

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ if (process.env.NODE_ENV === "production") {
 //Connect to the Mogno DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books",
     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
     }
 );
 
